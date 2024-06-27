75332ab7d3449c9151e6af82009d9b31d8fecf90
Sapadi <saptoadiyanto291@gmail.com>
2024-06-27 16:06:34 +0700

    add build

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = BASE_URL_API
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
     params: {
@@ -8,7 +8,7 @@ const options = {
     },
     headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': API_HOST
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
 
